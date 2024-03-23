be929fc21516ea6fad79627fdd729bfafd8a4704
kamal <kamal@lalokalabs.co>
2024-03-23 15:58:02 +0800

    Add delete button.
    - Add static files serving
    - Add unpoly
    - Add bootstrap

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ BASE_DIR = pathlib.Path(__file__).parent.absolute()
 sys.path.insert(0, str(BASE_DIR / "lib"))
 print(sys.path)
 
+from bottle import request
+from bottle import static_file
 from bottle import route, run, SimpleTemplate
 
 from peewee import *
@@ -25,7 +27,7 @@ class Person(Model):
     class Meta:
         database = db
 
-@route('/hello/<name>')
+@route('/hello/<name>', ["GET", "POST"])
 def index(name):
     try:
         person = Person.get(Person.name == name)
@@ -33,8 +35,17 @@ def index(name):
         person = Person(name=name)
         person.save()
 
+    if request.method == "POST":
+        if request.forms.get("submit", "") == "Yes":
+            person.delete_instance()
+
     people = Person.select()
     return render("index.html", name=name, people=people)
+
+@route('/static/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root=str(BASE_DIR / "static"))
+
 
 if __name__ == "__main__":
     db.create_tables([Person])
